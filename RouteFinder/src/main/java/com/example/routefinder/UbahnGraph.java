f5ad35d1ab0f62f368f5af852629c6cd92756601
Finn Clewes <147063819+AineClewes@users.noreply.github.com>
2025-04-26 17:40:43 +0100

    Update UbahnGraph.java

--- RouteFinder/src/main/java/com/example/routefinder/UbahnGraph.java
+++ RouteFinder/src/main/java/com/example/routefinder/UbahnGraph.java
@@ -4,18 +4,34 @@ import java.util.List;
 import java.util.Map;
 
 public class UbahnGraph {
-    Map<Station, List<Edge>> adjacencyList;
+   private Map<String, Station> stations;
 
-    public void addStation(Station s) {
-
+    public UbahnGraph() {
+        this.stations = new HashMap<>();
     }
 
-    public void addEdge(Station from, Station to, double distance, String lineName) {
-
+    public Station getOrCreateStation(String name) {
+        Station station = stations.get(name);
+        if (station == null) {
+            station = new Station(name);
+            stations.put(name, station);
+        }
+        return station;
     }
 
-    public void getNeighbours(Station s) {
+    public void addEdge(Station from, Station to, double distance, String lineName, String lineColour) {
+        Edge edge = new Edge(from, to, distance, lineName);
+        from.addNeighbour(edge);
 
+        Edge reverseEdge = new Edge(to, from, distance, lineName);
+        to.addNeighbour(reverseEdge);
     }
 
+    public Station getStation(String name) {
+        return stations.get(name);
+    }
+
+    public Collection<Station> getAllStations() {
+        return stations.values();
+    }
 }
